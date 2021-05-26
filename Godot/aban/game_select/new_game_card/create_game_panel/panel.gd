963be4a4e1230e0783391056135e8b465a1f27f7
Sarvarian <Sarvarian@Hotmail.com>
2021-05-27 00:00:47 +0430

    replace error handling code in creating_game_panel/panel.gd

--- Godot/aban/game_select/new_game_card/create_game_panel/panel.gd
+++ Godot/aban/game_select/new_game_card/create_game_panel/panel.gd
@@ -18,7 +18,8 @@ func _ready() -> void:
 	
 	if not directory.dir_exists(user_packages_directory):
 		err = directory.make_dir_recursive(user_packages_directory)
-	check_and_report_err(err)
+		if err:
+			Log.error("Creating User Packages Directory Failed With Godot Error Code: " + String(err))
 	
 	res_packages = check_dir_for_packages(res_packages_directory)
 	user_packages = check_dir_for_packages(user_packages_directory)
@@ -32,10 +33,12 @@ func check_dir_for_packages(dir : String) -> PoolStringArray:
 	var packages : PoolStringArray = []
 	
 	err = directory.open(dir)
-	check_and_report_err(err)
+	if err:
+		Log.error("Opennig Directory '" + dir + "' Failed With Godot Error Code: " + String(err))
 	
 	err = directory.list_dir_begin(true, false)
-	check_and_report_err(err)
+	if err:
+		Log.error("'" + dir + "'.list_dir_begin(true, false) Failed With Godot Error Code: " + String(err))
 	
 	var name := directory.get_next()
 	while name != "":
@@ -46,12 +49,6 @@ func check_dir_for_packages(dir : String) -> PoolStringArray:
 	directory.list_dir_end()
 	
 	return packages
-
-
-func check_and_report_err(err : int) -> void:
-	if not err:
-		return
-	err_label.text += "Godot Error code: {}\n".format([err], "{}")
 
 
 
