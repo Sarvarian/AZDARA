4a222944c06a39c83b4b378fac188b26388e6955
Sarvarian <Sarvarian@Hotmail.com>
2021-06-02 12:06:02 +0430

    Update tree.gd

--- Godot/aban/game_select/new_game_card/create_game_panel/tree.gd
+++ Godot/aban/game_select/new_game_card/create_game_panel/tree.gd
@@ -4,9 +4,8 @@ extends Tree
 func _ready() -> void:
 	var _root := create_item()
 	hide_root = true
-	for _i in range(50):
-		for scenario in PackageManager.scenarios:
-			var name := scenario as String
-			if name is String:
-				var item := create_item()
-				item.set_text(0, name)
+	for scenario in PackageManager.scenarios:
+		var name := scenario as String
+		if name is String:
+			var item := create_item()
+			item.set_text(0, name)
